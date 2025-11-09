6459e13f9057f94ed45a77970909dcc272311abe
shouravhasanshurjo8201 <shouravhasanshurjo8201@gmail.com>
2025-11-09 20:47:34 +0600

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1> React</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+     
+
+    </>
+  )
+}
+
+export default App
