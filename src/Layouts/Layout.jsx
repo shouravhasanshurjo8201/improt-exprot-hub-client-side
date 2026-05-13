f878592181851e96b8554ca2aadcadaa22b60c34
Shourav Hasan <shouravhasanshurjo8201@gmail.com>
2026-05-13 07:01:03 +0600

    Fix formatting in Layout component for improved readability

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,10 +1,9 @@
-
 import { Outlet } from 'react-router'
 import Footer from '../Components/Footer/Footer';
 import Navbar from '../Components/Navbar/Navbar';
 import { Toaster } from 'react-hot-toast';
 const Layout = () => {
- 
+
   return (
     <div className='flex flex-col min-h-screen'>
       <Navbar></Navbar>
